36c3ef35e6d81047ad05be1a96587a8829d288d8
sabatiny <sabatinystopa@gmail.com>
2021-09-21 21:33:09 -0300

    Adiciona objeto coletável

---
+++ Assets/Scripts/Collectable/Collectable.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace PlatformGame{
+    public class Collectable : MonoBehaviour{
+        [SerializeField] CollecatableKind collecatableKind;
+        void OnTriggerEnter(Collider other) {
+            if(other.CompareTag(Constants.PLAYER_TAG))  
+                Collect();  
+        }
+
+        void Collect(){
+            if(collecatableKind == CollecatableKind.MainCristal){
+                GameManager.instance.hasMainCollectable = true;
+                Destroy(gameObject);
+            }
+            else
+                GameManager.instance.SumCommonCristals();
+        }
+    }
+}
