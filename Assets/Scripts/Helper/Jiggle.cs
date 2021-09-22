aefeb6585916315b11e23e9e49b0512e9fd9b302
sabatiny <sabatinystopa@gmail.com>
2021-09-22 08:34:54 -0300

    Adicionando jiggle para objetos

---
+++ Assets/Scripts/Helper/Jiggle.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+namespace PlatformGame.Helper{
+    public class Jiggle : MonoBehaviour{
+        [Header("Velocidades")]
+        [SerializeField] float rotationSpeed = 5;
+        [SerializeField] float upAndDownSpeed = 5;
+        Vector3 startPosition;
+        void Start(){
+            startPosition = transform.position;    
+        }
+    
+        void Update(){
+            transform.position = new Vector3(transform.position.x, startPosition.y + Mathf.Sin(Time.time * upAndDownSpeed), transform.position.z);
+        }
+    }
+}
