34828a502efdc2f2424dcbe1e49df4495b4238d1
sabatiny <sabatinystopa@gmail.com>
2021-09-22 12:52:35 -0300

    Removendo e adicionando pacotes

--- Assets/Scripts/Helper/Jiggle.cs
+++ Assets/Scripts/Helper/Jiggle.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 namespace PlatformGame.Helper{
     public class Jiggle : MonoBehaviour{
         [Header("Velocidades")]
-        [SerializeField] float rotationSpeed = 5;
+        // [SerializeField] float rotationSpeed = 5;
         [SerializeField] float upAndDownSpeed = 5;
         Vector3 startPosition;
         void Start(){
