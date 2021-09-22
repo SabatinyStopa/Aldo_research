b92cb174279eb44dc3229b0a99a2f1997f8b6eb5
sabatiny <sabatinystopa@gmail.com>
2021-09-22 20:04:58 -0300

    Adiciona efeito de particula para quando destrói inimigo

--- Assets/Scripts/Health/EnemyHealth.cs
+++ Assets/Scripts/Health/EnemyHealth.cs
@@ -3,6 +3,8 @@ namespace PlatformGame{
     public class EnemyHealth : Health{
         [Header("Parâmetro")]
         [SerializeField] string triggerHitName = "TakeDamage";
+        [Header("Prefab para morte")]
+        [SerializeField] GameObject plasmaExplosionPrefab;
         Animator animator;
 
         void Start(){
@@ -13,6 +15,11 @@ namespace PlatformGame{
         public override void TakeDamage(float amount){
             base.TakeDamage(amount);
             animator.SetTrigger(triggerHitName);
+        }
+
+        public override void Die(){
+            Instantiate(plasmaExplosionPrefab, transform.position + Vector3.up / 2, Quaternion.identity);
+            base.Die();
         }
     }
 }
